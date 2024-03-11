11ca64b9fcbebbeca809e2cfcbc869767294e4bf
LAPTOP-2OJ49QLA\user <p396520@gmail.com>
2024-03-11 23:00:05 +0800

    teamplates章節完成

--- food/views.py
+++ food/views.py
@@ -4,16 +4,24 @@ from .models import Item
 from django.template import loader
 # Create your views here.
 
+
 def index(request):
     item_list = Item.objects.all()
     template = loader.get_template("food/index.html")
     context = {
-        "item_list": item_list
+        "item_list": item_list,
     }
     return render(request, "food/index.html", context)
+
 
 def item(request):
     return HttpResponse("<h1>This is an item view</h1>")
 
+
 def detail(request, item_id):
-    return HttpResponse("This is item no/id: %s" % item_id)
+    item = Item.objects.get(pk=item_id)
+    context = {
+        "item": item,
+    }
+
+    return render(request, "food/detail.html", context)
