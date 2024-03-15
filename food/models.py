9f943c13bab9ef771ecad65eb098e7f4da00be1a
duncanli <duncanli@hotaiconnected.com.tw>
2024-03-15 17:03:53 +0800

    static file & site design

--- food/models.py
+++ food/models.py
@@ -9,3 +9,4 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500,default="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg")
